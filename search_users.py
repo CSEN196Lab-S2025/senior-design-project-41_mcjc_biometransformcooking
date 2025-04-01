392e890e19b814b7d3b3566f304e89f9b37eacb0
Jerry Chen <52337021+jchyjc@users.noreply.github.com>
2025-03-31 17:56:47 -0700

    Update search_users.py

--- search_users.py
+++ search_users.py
@@ -1,24 +1,14 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Load the service account key
-cred = credentials.Certificate("/Users/jianchen/Desktop/BIOME_Food_App/BIOME_FirebaseDB_Key.json")
-
-# Initialize Firebase with the Firestore DB
+cred = credentials.Certificate("key.json")
 firebase_admin.initialize_app(cred)
-
-# Reference Firestore client
 db = firestore.client()
 
-# Reference the "users" collection in Firestore and retrieve the document for "jerry_chen"
 user_ref = db.collection("users").document("jerry_chen")
-
-# Fetch the document
 user_doc = user_ref.get()
 
-# Check if the document exists
 if user_doc.exists:
-    # Document exists, print the data
     print(f"User Data: {user_doc.to_dict()}")
 else:
     print("User not found.")
